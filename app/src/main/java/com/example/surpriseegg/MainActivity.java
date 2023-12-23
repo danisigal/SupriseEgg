d2b267358709d137be0d9bbe7cdee114de575418
danisigal <dani.sigal1206@gmail.com>
2023-12-23 14:26:10 +0200

    counter changes in main

--- app/src/main/java/com/example/surpriseegg/MainActivity.java
+++ app/src/main/java/com/example/surpriseegg/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.surpriseegg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counter = 10;
+        TextView counterTextView = findViewById(R.id.textView_counter);
+        counterTextView.setText(counter);
     }
 }
---
+++ app/src/main/java/com/example/surpriseegg/Textview.java
@@ -0,0 +1,4 @@
+package com.example.surpriseegg;
+
+public class Textview {
+}
